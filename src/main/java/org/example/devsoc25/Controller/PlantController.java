6119c0608ee6767fdf5687ab16d328c412859413
HS-246 <happyhusain12@gmail.com>
2025-02-04 16:33:56 +0530

    Now when the server gets a plant image from app it should automatically call the AI backend and get its name and store that as well

--- src/main/java/org/example/devsoc25/Controller/PlantController.java
+++ src/main/java/org/example/devsoc25/Controller/PlantController.java
@@ -1,24 +1,33 @@
 package org.example.devsoc25.Controller;
 
 import org.example.devsoc25.entity.Plant;
-import org.example.devsoc25.service.plantService;
-import org.springframework.http.ResponseEntity;
+import org.example.devsoc25.service.PlantService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/app")
 public class PlantController {
 
-    private final plantService PlantService;
+    private final PlantService plantService;
 
-    public PlantController(plantService plantService) {
-        this.PlantService = plantService;
+    public PlantController(org.example.devsoc25.service.PlantService plantService) {
+        this.plantService = plantService;
     }
 
     @PostMapping("/{userid}/save")
-    public ResponseEntity<?> savePlant(@RequestBody Plant plant,@PathVariable long userid) {
-        Plant resplant=PlantService.savePlant(plant,userid);
+    public HttpStatus savePlant(@RequestBody Plant plant, @PathVariable long userid) throws URISyntaxException, IOException, InterruptedException {
+        Plant resplant=plantService.savePlant(plant,userid);
+        return HttpStatus.OK;
+    }
 
-        return ResponseEntity.ok(resplant);
+    @GetMapping("/{userid}/allplants")
+    public List<Plant> getAllPlants(@PathVariable long userid) {
+        List<Plant> resList=plantService.getAll(userid);
+        return resList;
     }
 }
---
+++ src/main/java/org/example/devsoc25/service/PlantService.java
@@ -0,0 +1,60 @@
+package org.example.devsoc25.service;
+
+import com.google.gson.Gson;
+import org.example.devsoc25.entity.Plant;
+import org.example.devsoc25.entity.User;
+import org.example.devsoc25.repository.PlantRepository;
+import org.example.devsoc25.repository.UserRepository;
+import org.springframework.boot.json.GsonJsonParser;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.Optional;
+import java.util.logging.Logger;
+
+@Service
+public class PlantService {
+    private static final Logger log = Logger.getLogger(UserService.class.getName());
+    private final PlantRepository plantRepository;
+    private final UserRepository userRepository;
+
+    public PlantService(PlantRepository plantRepository, UserRepository userRepository) {
+        this.plantRepository = plantRepository;
+        this.userRepository = userRepository;
+    }
+
+
+    public Plant savePlant(Plant plant,long userId) throws URISyntaxException, IOException, InterruptedException {
+        Optional<User> user = userRepository.findById(userId);
+        plant.setUser(user.get());
+
+        Gson gson = new Gson();
+        String jsonRequest = gson.toJson(plant.getImage());
+
+        HttpRequest request= HttpRequest.newBuilder()
+                .uri(new URI("${AI_URL}/image-classify"))
+                .POST(HttpRequest.BodyPublishers.ofString(jsonRequest))
+                .build();
+
+        HttpClient httpClient = HttpClient.newHttpClient();
+
+        HttpResponse<String> response=httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        String plantName = gson.fromJson(response.body(),String.class);
+        plant.setName(plantName);
+
+        return plantRepository.save(plant);
+    }
+
+    public List<Plant> getAll(long userid) {
+        Optional<List<Plant>> plants =plantRepository.findAllByUserUserid(userid);
+        //log.info(plants.get().toString());
+        return plants.get();
+    }
+}
